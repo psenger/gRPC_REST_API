60f85663c6bcf680b05ad3d507503c9016404c09
Philip A Senger <philip.a.senger@cngrgroup.com>
2018-08-19 19:57:29 +1000

    working script

--- script/build.sh
+++ script/build.sh
@@ -1,17 +1,21 @@
-#!/usr/bin/env bash
+
+cd ..
+export GOPATH=$(pwd)
+export PATH=$PATH:$GOPATH:$GOPATH/bin:$GOPATH/bin/include
 
 mkdir src && cd src
 mkdir github.com && cd github.com
 mkdir psenger && cd psenger
 mkdir gRPC_REST_API && cd gRPC_REST_API
 
-mv ../../../../Gopkg.lock ./
-mv ../../../../Gopkg.toml ./
-mv ../../../../main.go ./
-mv ../../../../README.md ./
-mv ../../../../api ./
-mv ../../../../server ./
-cd ../../../../
-export GOPATH=$(pwd)
-export PATH=$PATH:$GOPATH:$GOPATH/bin:$GOPATH/bin/include
-go build -o main ./src/github.com/psenger/main.go
+cd $GOPATH
+
+mv Gopkg.lock ./src/github.com/psenger/gRPC_REST_API/
+mv Gopkg.toml ./src/github.com/psenger/gRPC_REST_API/
+mv main.go    ./src/github.com/psenger/gRPC_REST_API/
+mv README.md  ./src/github.com/psenger/gRPC_REST_API/
+mv api        ./src/github.com/psenger/gRPC_REST_API/
+mv server     ./src/github.com/psenger/gRPC_REST_API/
+mv vendor     ./src/github.com/psenger/gRPC_REST_API/
+
+go build -o main ./src/github.com/psenger/gRPC_REST_API/main.go
